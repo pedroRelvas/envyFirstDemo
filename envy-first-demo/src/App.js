0060d7824022aaec7d091c290a245c00bb776321
F3MDEV <josepedrobrelvas@outlook.pt>
2022-10-09 02:32:24 +0100

    func: startMenu with animation, react-spring implemented

--- envy-first-demo/src/App.js
+++ envy-first-demo/src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import styles from './styles/styles.module.scss';
 import { useState } from 'react';
 import StartMenu from './components/StartMenu/StartMenu'
+
+
 function App() {
 
   const [mode, setMode] = useState('start');
 
+  
   return (
     <div className={styles.main}>
       {mode == 'start' && <StartMenu onStartClick={() => setMode('city')}></StartMenu>}
